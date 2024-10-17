a746fd9fb37cb9533a0f205c9c976cb912d4b78c
ywu256 <ywu256@my.centennialcollege.ca>
2024-10-16 23:42:38 -0400

    Remove unused code

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginPage from './Screens/LoginPage';
@@ -12,18 +10,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="Login" component={LoginPage} options={{ headerShown: false }}  />
         <Stack.Screen name="PatientList" component={PatientListPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

36a26ae881ef5c07dd2d512ae84b4ec98496ea5c
ywu256 <ywu256@my.centennialcollege.ca>
2024-11-22 21:09:19 -0500

    jest setup

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    preset: "react-native",
+    setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+    },
+    transformIgnorePatterns: [
+        "node_modules/(?!(react-native|@react-native|react-native-elements|@react-native-elements/themed)/)"
+    ],
+};
+  

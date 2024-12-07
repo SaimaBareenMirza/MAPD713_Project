6000d7198598fa6dc09ec7fa25b384bebd01bf98
Saima <bareensaima2512@gmail.com>
2024-12-06 23:59:33 -0500

    added 404 error

--- API/server.js
+++ API/server.js
@@ -679,6 +679,12 @@ app.get('/clinical/:patientId', async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 });
+
+// Catch-all route for 404
+app.use((req, res, next) => {
+  res.status(404).send('Not Found');
+});
+
 // Start the server
 app.listen(PORT,() => {
   console.log(`Server running on http://localhost:${PORT}`);
